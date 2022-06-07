553575852f831fdae076ae567c478191089048a0
Lucas Queiroga <lucasqueirogadev@gmail.com>
2022-06-07 10:54:47 -0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+
+const IndexPage: NextPage = () => {
+  return (
+    <>
+      <h1>Hello, world</h1>
+      <p>Olá, mundo</p>
+    </>
+  );
+};
+
+export default IndexPage;
