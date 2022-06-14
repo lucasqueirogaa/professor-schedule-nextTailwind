13554584639432fbcc42da1c9da0507ea22a2b23
Lucas Queiroga <lucasqueirogadev@gmail.com>
2022-06-14 08:59:50 -0300

    Create auth with next-auth & auth0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
 import { NextPage } from 'next';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 const IndexPage: NextPage = () => {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+
   return (
-    <>
-      <h1>Hello, world</h1>
-      <p>Olá, mundo</p>
-    </>
+    <div className="text-3xl">
+      Not signed in <br />
+      <button onClick={() => signIn('auth0')}>Sign in</button>
+    </div>
   );
 };
 
